dae15ee7aeb0d98c9542b89359eee93707a15480
Mohd Irtefa <irtefa1@illinois.edu>
2012-12-28 18:15:13 -0600

    added frontend validation

--- static/js/signup.js
+++ static/js/signup.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
     $('#signup-button').click(function(event) {
+
+        validate_entry("username");
+        validate_entry("password");
+        validate_entry("useremail");
+        validate_entry("confirm");
+
         if (is_valid_form())
         {
             $('#invalid-form').addClass('hidden');
@@ -27,6 +33,81 @@ function signup(event) {
         "pass_word": pass_word
     };
     console.log(post_data);
+}
+
+function validate_entry(id)
+{
+    if (id==="username")
+    {
+        var u_name = $("#user-name").val();
+        if (u_name.length < 6 || u_name.length > 13)
+        {
+            $("#username-group").removeClass("success");
+            $("#username-help").removeClass("hidden");
+            $("#username-group").addClass("error");
+        }
+        else
+        {
+            $("#username-group").removeClass("error");
+            $("#username-group").addClass("success");
+            $("#username-group").addClass("hidden");
+        }
+    }
+
+    else if (id==="password")
+    {
+        var pwd = $("#pass-word").val();
+        if (pwd.length < 6 || pwd.length > 13)
+        {
+            $("#password-group").removeClass("success");
+            $("#password-help").removeClass("hidden");
+            $("#password-group").addClass("error");
+        }
+        else
+        {
+            $("#password-group").removeClass("error");
+            $("#password-group").addClass("success");
+            $("#password-help").addClass("hidden");
+        }
+    }
+
+    else if(id==="useremail")
+    {
+        var email = $("#user-email").val();
+        var pattern = /^([a-zA-Z0-9_.-])+@([a-zA-Z0-9_.-])+\.([a-zA-Z])+([a-zA-Z])+/;
+        if (pattern.test(email))
+        {
+            $("#useremail-group").removeClass("error");
+            $("#useremail-group").addClass("success");
+            $("#useremail-help").addClass("hidden");
+        }
+        else
+        {
+            $("#useremail-group").removeClass("success");
+            $("#useremail-help").removeClass("hidden");
+            $("#useremail-group").addClass("error");
+        }
+    }
+    else if(id==="confirm")
+    {
+        var pd = $("#password").val();
+        var cnf  = $("#confirm-pass").val();
+
+        if(pd === cnf)
+        {
+             $("#confirm-group").removeClass("error");
+            $("#confirm-group").addClass("success");
+            $("#confirm-help").addClass("hidden");
+        }
+        else
+        {
+            $("#confirm-group").removeClass("success");
+            $("#confirm-help").removeClass("hidden");
+            $("#confirm-group").addClass("error");
+        }
+
+    }
+
 }
 
 function is_valid_form() {
