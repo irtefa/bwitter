6f1d4796991fd74c064230d32ec20e2e57532a6f
Mohd Irtefa <irtefa1@illinois.edu>
2012-12-29 00:02:11 -0600

    signup handler not causing exceptions anymore

--- handlers/user.py
+++ handlers/user.py
@@ -20,6 +20,7 @@ class SignupHandler(RequestHandler):
         if result is not None:
             self.write({"success": "username"})
             self.finish()
+            return
 
         select_user_email = """SELECT `user_email` FROM `User` WHERE `user_email` = "%s" """\
         % (user["user_email"])
@@ -28,11 +29,15 @@ class SignupHandler(RequestHandler):
         if result is not None:
             self.write({"success": "useremail"})
             self.finish()
+            return
 
-        add_user_info = """INSERT INTO `User` (`user_name`, `user_email`, `first_name`, `last_name`, `password`) VALUES ("%s", "%s", "%s","%s","%s")"""\
-        % (user["user_name"], user["user_email"], user["first_name"], user["last_name"], hash)
+        if result is None:
+            add_user_info = """INSERT INTO `User` (`user_name`, `user_email`, `first_name`, `last_name`, `password`) VALUES ("%s", "%s", "%s","%s","%s")"""\
+            % (user["user_name"], user["user_email"], user["first_name"], user["last_name"], hash)
 
-        result = self.application.db.execute(add_user_info)
+            result = self.application.db.execute(add_user_info)
 
-        self.write({"success": "true"})
-        self.finish()
+            self.write({"success": "true"})
+            self.finish()
+        else:
+            self.finish()
